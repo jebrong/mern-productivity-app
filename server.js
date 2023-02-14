b941fe7a7fa7a303e9b62c0643fe55f84733e145
jebrong <92987206+jebrong@users.noreply.github.com>
2023-02-15 03:58:53 +0800

    change favicon, remove lock

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-// app.use(
-//   cors({
-//     // origin: true,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    // origin: true,
+    credentials: true,
+  })
+);
 
 const userRouter = require("./routes/userRoutes");
 const todoRouter = require("./routes/todoRoutes");
